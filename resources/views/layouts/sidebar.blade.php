ee3c332f69f83cdd880593ca98f154c1749d9b17
Yannick G <ygouede@gmail.com>
2020-05-24 14:01:39 +0000

    version_ok_avant_test
    mise à jour lien image user
    ajout transporteur.json
    version stable avant test

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="http://infyom.com/images/logo/blue_logo_150x150.jpg" class="img-circle"
+                <img src="{{ asset('assets/images/avatars/avatar.png') }}" class="img-circle"
                      alt="User Image"/>
             </div>
             <div class="pull-left info">
